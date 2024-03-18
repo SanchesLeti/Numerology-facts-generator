7040af8aa5d45996499a85a5ef1bdd95c96d86fa
SanchesLeti <145586269+SanchesLeti@users.noreply.github.com>
2024-03-18 22:13:42 +0000

    Basic styling and added API intergration

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+function displayAnswer(response) {
+  new Typewriter("#numerology-answer", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generateanswer(event) {
   event.preventDefault();
 
-  new Typewriter("#numerology-answer", {
-    strings: ["The answer is going to be here"],
-    autoStart: true,
-    delay: 1,
-      cursor: "",
-  });
+  let userInstructions = document.querySelector("#userAnswer");
+  let apiKey = "5bc8eo589961bd06a04fb3t787d4b1a2";
+  let context =
+    "You are a numerology AI expert who summs the numbers of a birthday and gives a life path number explanition in basic html form and separete each line with <br>. Please make sure to provide direct, simple and friendly answers and use emojis. Please use the user instructions";
+  let prompt = `User instructions: Generate a random numerology fact using the date of birth given by ${userInstructions.value}`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Please wait...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayAnswer);
 }
 
 let numerologyFormula = document.querySelector("#num-generator-form");
